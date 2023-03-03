594840e5e466b1c2812ab2a98364826837d2634a
MexL <madiajijah7@gmail.com>
2023-03-03 22:32:26 +0800

    fix api

--- src/lib/ingredients.ts
+++ src/lib/ingredients.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export const getIngredients = async () => {
   const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/list.php?i=list`
+    `${process.env.NEXT_PUBLIC_API_URL}/api/json/v1/1/list.php?i=list`
   );
   return data.meals;
 };
 
 export const getIngredient = async (name: string) => {
   const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/filter.php?i=${name}`
+    `${process.env.NEXT_PUBLIC_API_URL}/api/json/v1/1/filter.php?i=${name}`
   );
   return data.meals;
 };
--- src/lib/meals.ts
+++ src/lib/meals.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getMeal = async (id: BigInteger) => {
   const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/lookup.php?i=${id}`
+    `${process.env.NEXT_PUBLIC_API_URL}/api/json/v1/1/lookup.php?i=${id}`
   );
   return data.meals;
 };
